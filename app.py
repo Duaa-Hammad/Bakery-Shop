16de31913d58e6e9efbfb788023c34811e3ecf64
Duaa Hammad <doaahammad211@gmail.com>
2025-06-08 19:29:09 +0200

    Managed Routes in Flask

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def home():
+    return render_template("index.html", sentence = "Sign Up Now", username = None)
+
+
+@app.route("/index/<username>")
+def index(username):
+    return render_template("index.html", sentence=username)
+
+
+@app.route("/sign_up")
+def sign_up():
+    return render_template("sign_up.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        username = request.form["username"]
+        password = request.form["password"]
+        if username == "duaahammad" and password == "dpass":
+            return redirect(url_for("index", username=username))
+        else:
+            return redirect(url_for("sign_up"))
 
 
 app.run(debug=True)
